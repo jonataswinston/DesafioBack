2d96b81b3eb3fb83367632abcbc2bc6cd9d6bef4
Jonataswinston <jonataswinston@gmail.com>
2017-10-25 00:08:27 -0200

    //tesr

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ import static main.InsertData.insertCustomerAccount;
 public class Main {
 
     public static void main(String[]args) {
+        //teste
         Scanner ler = new Scanner(System.in);
         String res = "";
         do {
